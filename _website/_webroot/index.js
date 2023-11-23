ef2459b7bd1b942f512c6291d1b4512b5ebb3bda
Ben Feist <bf@benfeist.com>
2023-11-23 10:11:07 -0500

    prettier; forum link

--- _website/_webroot/index.js
+++ _website/_webroot/index.js
@@ -1,21 +1,22 @@
-$( document ).ready(function() {
-    var webFontConfig = {
-        google: {
-            families: ['Michroma',
-                'Oswald:300,400,700',
-                'Roboto Mono:200,400,500,700',
-                'Roboto Slab:300']
-        },
-        active: function() {
-            console.log("INIT: fonts loaded");
-        }
-    };
-    WebFont.load(webFontConfig);
-    $('body').addClass('app-ready');
-    console.log( "ready!" );
+$(document).ready(function () {
+  var webFontConfig = {
+    google: {
+      families: [
+        "Michroma",
+        "Oswald:300,400,700",
+        "Roboto Mono:200,400,500,700",
+        "Roboto Slab:300",
+      ],
+    },
+    active: function () {
+      console.log("INIT: fonts loaded");
+    },
+  };
+  WebFont.load(webFontConfig);
+  $("body").addClass("app-ready");
+  console.log("ready!");
 });
 
-
 function missionClick(url) {
-    window.location.href = url;
+  window.location.href = url;
 }
