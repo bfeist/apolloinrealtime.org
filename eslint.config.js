f167618b604908476282246a8e7fc82008683bb0
Ben Feist <benjamin.f.feist@nasa.gov>
2026-05-28 16:36:30 -0400

    fix(lint): suppress tseslint.config deprecation; ESLint 10 has no ESM defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,11 @@
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
+// tseslint.config() is flagged as @deprecated by typescript-eslint's own
+// no-deprecated rule now that eslint.config.js is type-checked, but ESLint 10
+// doesn't export defineConfig as an ESM named binding yet. Suppress here;
+// revisit when ESLint ships a proper ESM export for defineConfig.
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 export default tseslint.config(
   {
     ignores: [
